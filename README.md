e64538e98408a061cee3086c04dd0c35ac10d68e
Neetx <antoniopastorelli2@gmail.com>
2018-05-17 11:50:17 +0200

    Update
    Missing stdin detection.

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
 ShellcodeExtractor
+========
 
-Python script for extracting shellcode from object file.
+SHELLCODE-EXTRACTOR
+Tool used to extract shellcode and lenght from an object/binary file.
+
+Copyright (C) 2017  Neetx
+
+This file is part of Shellcode-Extractor.
+
+Shellcode-Extractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Shellcode-Extractor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+###CONTACTS:
+[Neetx](mailto:neetx@protonmail.com)
+
+---
+Tool developed for study and research reasons, I hope it will be useful.
+
+###Usage
 
 Write assembly code, product an object file and the use this script in pipeline to objdump.
 
@@ -8,6 +35,7 @@ Example:
 
 Let's write spwaning shell assembly code in spawnshell.asm ( NASM )
 
+```
 	xor eax, eax,
 	push eax
 	push 0x68732f2f
@@ -19,11 +47,22 @@ Let's write spwaning shell assembly code in spawnshell.asm ( NASM )
 	mov ecx, esp
 	mov al, 11
 	int 0x80
+```
 
-Launch : nasm -f elf spawnshell.asm -o output.o
+Launch : 
+```
+nobody@nobody:~$ nasm -f elf spawnshell.asm -o output.o
+```
 
-Now we can use our script: objdump -d output.o | python shellcode_extractor.py
+Now we can use our script: 
+```
+nobody@nobody:~$ objdump -d output.o | python shellcode_extractor.py
 
-The output is:	\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80
+\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80
 
+Lenght: 1822
+```
+---
+###Testing:
 
+Insert our shellcode into the empty string in shellcode_tester.c, compile it with gcc and run.
--- shellcode_extractor.py
+++ shellcode_extractor.py
@@ -1,23 +1,53 @@
-"""Neetx ShellcodeExtractor"""
+"""
+SHELLCODE-EXTRACTOR
+Tool used to extract shellcode and lenght from an object/binary file.
+
+Copyright (C) 2017  Neetx
+
+This file is part of Shellcode-Extractor.
+
+Shellcode-Extractor is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Shellcode-Extractor is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+CONTACTS:
+    - neetx@protonmail.com
+"""
+
 import sys, re
 
-try:
-    shellcode = ""
-    while True:
-        item = sys.stdin.readline()
-        if item:
-            if re.match("^[ ]*[0-9a-f]*:.*$",item):
-                item =item.split(":")[1].lstrip()
-                x = item.split("\t")
-                opcode = re.findall("[0-9a-f][0-9a-f]",x[0])
-                for i in opcode:
-                    shellcode += "\\x" + i
-        else: 
-            break
-    if shellcode == "":
-        print "Nothing to extract"
-    else:    
-        print shellcode
-except:
-    print "Error! \n Usage:  objdump -d example.o | python shellcode_extractor.py" 
-    pass
+if not sys.stdin.isatty():
+    try:
+        shellcode = ""
+        lenght = 0
+        while True:
+            item = sys.stdin.readline()
+            if item:
+                if re.match("^[ ]*[0-9a-f]*:.*$",item):
+                    item =item.split(":")[1].lstrip()
+                    x = item.split("\t")
+                    opcode = re.findall("[0-9a-f][0-9a-f]",x[0])
+                    for i in opcode:
+                        shellcode += "\\x" + i
+                        lenght += 1
+            else: 
+                break
+        if shellcode == "":
+            print "Nothing to extract"
+        else:    
+            print "\n" + shellcode
+            print "\nLenght: " + str(lenght) + "\n"
+    except:
+        print "\nError! \n Usage:  objdump -d example.o | python shellcode_extractor.py" 
+        pass
+else:
+    print "\nError! \n Usage:  objdump -d example.o | python shellcode_extractor.py \n" 
